8dc02db30b2ece18d8b62265d76184944c8a6c12
Zack Jones <zackjones11@gmail.com>
2020-05-11 16:18:23 +0200

    initial boilerplate vuejs, webpack, eslint

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+import "./variables.css";
+import "./main.css";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
