bc8aa0ee5934863696c240496b403619a7ec465e
Zack Jones <zackjones11@gmail.com>
2020-05-17 20:42:58 +0200

    base button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <button :class="$style.button" :disabled="disabled" @click="onClick">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    onClick(event) {
+      this.$emit("click", event);
+    }
+  }
+};
+</script>
+
+<style module>
+.button {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 25px;
+  min-width: 150px;
+  font-weight: var(--weight-bold);
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  background: transparent;
+  color: var(--light-300);
+  border: 2px solid var(--light-300);
+  cursor: pointer;
+  transition: 0.1s background-color, color ease-in;
+}
+.button:hover {
+  background-color: var(--light-300);
+  color: var(--dark-300);
+}
+</style>
