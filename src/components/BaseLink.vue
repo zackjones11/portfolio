868a916c71d23a7c16f511bace7cf5a7246a2263
Zack Jones <zackjones11@gmail.com>
2020-05-17 21:35:33 +0200

    base link component for external links

---
+++ src/components/BaseLink.vue
@@ -0,0 +1,28 @@
+<template>
+  <a :class="$style.link" :href="href" :target="target">
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  name: "BaseLink",
+  props: {
+    href: {
+      type: String,
+      required: true
+    },
+    target: {
+      type: String,
+      validator: prop =>
+        ["_blank", "_self", "_parent", "_top", "framename"].includes(prop)
+    }
+  }
+};
+</script>
+
+<style module>
+.link {
+  text-decoration: none;
+}
+</style>
