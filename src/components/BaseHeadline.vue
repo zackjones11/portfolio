c915f3d561fddb6e5569ff4f64d851730c31fa2f
Zack Jones <zackjones11@gmail.com>
2020-05-17 21:57:48 +0200

    base headline component accepting levels prop

---
+++ src/components/BaseHeadline.vue
@@ -0,0 +1,34 @@
+<template>
+  <component :class="$style.headline" v-bind:is="`h${level}`">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  name: "BaseLink",
+  props: {
+    level: {
+      type: Number,
+      required: true,
+      validator: prop => prop <= 6
+    }
+  }
+};
+</script>
+
+<style module>
+.headline {
+  text-transform: uppercase;
+  letter-spacing: 4px;
+}
+
+.headline:after {
+  content: "";
+  display: block;
+  width: 5%;
+  height: 3px;
+  margin-top: 4px;
+  background-color: var(--light-300);
+}
+</style>
