a92b9519608daa15a2e1f2764d006508f8b0985b
Zack Jones <zackjones11@gmail.com>
2020-05-17 15:35:48 +0200

    init vue prism

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VuePrism from "vue-prism";
 
 import "./variables.css";
 import "./main.css";
 
+Vue.use(VuePrism);
 Vue.config.productionTip = false;
 
 new Vue({
