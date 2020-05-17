51ecc41e5c0eb3f476f31a1da41c4ff6f6c81a91
Zack Jones <zackjones11@gmail.com>
2020-05-17 20:41:03 +0200

    svg loader

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,18 @@ module.exports = {
       .test(/\.txt$/)
       .use("raw-loader")
       .loader("raw-loader");
+
+    config.module.rules.delete("svg");
+  },
+
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.svg$/,
+          loader: "vue-svg-loader"
+        }
+      ]
+    }
   }
 };
