e73b22d7ac8234ad7c10ae302a3b7ebfe62adf51
Zack Jones <zackjones11@gmail.com>
2020-05-17 16:52:15 +0200

    globally load all components with BaseXXX.vue

--- src/main.js
+++ src/main.js
@@ -8,6 +8,20 @@ import "./main.css";
 Vue.use(VuePrism);
 Vue.config.productionTip = false;
 
+const registerGlobalComponents = components => {
+  components.keys().forEach(key => {
+    const name = key.match(/([a-z0-9]+)\./i)[1];
+    Vue.component(name, components(key).default);
+  });
+};
+
+const components = require.context(
+  "./components",
+  true,
+  /Base[a-z0-9]+\.(vue)$/i
+);
+registerGlobalComponents(components);
+
 new Vue({
   render: h => h(App)
 }).$mount("#app");
