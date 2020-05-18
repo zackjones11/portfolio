eaa471ebc15afe443b024e9a734799c8e7e6eed2
Zack Jones <zackjones11@gmail.com>
2020-05-18 10:26:14 +0200

    added professional me copy

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -4,24 +4,53 @@
     <div :class="$style.editor__content">
       <CodeEditorFile v-bind:is-first="true" :code="introInCode" />
       <CodeEditorFile>
-        <BaseHeadline :level="3">Social</BaseHeadline>
-        <div :class="$style.editor__socialButtons">
-          <BaseLink href="https://github.com/zackjones11" target="_blank">
-            <BaseButton>
-              <BaseIcon name="github" />
-              Github
-            </BaseButton>
-          </BaseLink>
+        <div :class="$style.editor__block">
+          <BaseHeadline :level="3">Professional Me</BaseHeadline>
+          <p>
+            I am a developer with over 6 years' experience mostly working with
+            frontend technologies on a variety of exciting projects. I have
+            worked with agencies such as
+            <BaseLink href="https://6rs.co.uk" target="_blank"
+              >sixredsquares</BaseLink
+            >
+            and
+            <BaseLink href="http://nodelondon.com" target="_blank"
+              >node london</BaseLink
+            >, small start-ups like
+            <BaseLink href="https://www.thebloggerprogramme.com" target="_blank"
+              >The Blogger Programme</BaseLink
+            >
+            and larger corporations including
+            <BaseLink href="https://www.skygroup.sky" target="_blank"
+              >Sky UK</BaseLink
+            >
+            and
+            <BaseLink href="https://aboutyou.de" target="_blank"
+              >ABOUT YOU GmbH</BaseLink
+            >.
+          </p>
+        </div>
 
-          <BaseLink
-            href="https://www.linkedin.com/in/zack-jones-6b76802b/"
-            target="_blank"
-          >
-            <BaseButton>
-              <BaseIcon name="linkedin" />
-              LinkedIn
-            </BaseButton>
-          </BaseLink>
+        <div :class="$style.editor__block">
+          <BaseHeadline :level="3">Social Me</BaseHeadline>
+          <div :class="$style.editor__socialButtons">
+            <BaseLink href="https://github.com/zackjones11" target="_blank">
+              <BaseButton>
+                <BaseIcon name="github" />
+                Github
+              </BaseButton>
+            </BaseLink>
+
+            <BaseLink
+              href="https://www.linkedin.com/in/zack-jones-6b76802b/"
+              target="_blank"
+            >
+              <BaseButton>
+                <BaseIcon name="linkedin" />
+                LinkedIn
+              </BaseButton>
+            </BaseLink>
+          </div>
         </div>
       </CodeEditorFile>
     </div>
@@ -62,6 +91,10 @@ export default {
   display: flex;
   flex-direction: row;
   height: 100%;
+}
+
+.editor__block {
+  margin-bottom: 45px;
 }
 
 .editor__socialButtons {
