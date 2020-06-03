a10d44212b28344263043cf8a5bdce40fb6271ff
Zack Jones <zackjones11@gmail.com>
2020-06-03 21:06:15 +0200

    hasBitSelf unit test

---
+++ src/components/SnakeGame/helpers/hasBitSelf.test.js
@@ -0,0 +1,13 @@
+import { hasBitSelf } from "./";
+
+describe('hasBitSelf.js', () => {
+  it('returns false if snake does not include row and column', () => {
+    const snake = [{ row: 2, column: 5 }];
+    expect(hasBitSelf(snake, {row: 1, column: 3})).toBeFalsy();
+  });
+
+  it('returns true if snake does include row and column', () => {
+    const snake = [{ row: 2, column: 5 }];
+    expect(hasBitSelf(snake, {row: 2, column: 5})).toBeTruthy();
+  });
+});
