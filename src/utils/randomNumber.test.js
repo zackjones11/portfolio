1189f82a28d5a2ccbe10f7dfe6f3ff804cf5de90
Zack Jones <zackjones11@gmail.com>
2020-06-03 20:03:17 +0200

    unit tests for randomNumber

---
+++ src/utils/randomNumber.test.js
@@ -0,0 +1,16 @@
+import { randomNumber } from "./";
+
+describe('randomNumber.js', () => {
+  it('returns a number', () => {
+    const random = randomNumber({ min: 1, max: 100 });
+    expect(typeof random).toBe('number');
+  });
+
+  it('returns a number in-between min and max args', () => {
+    const args = { min: 10, max: 100 };
+    const random = randomNumber(args);
+
+    expect(random).toBeGreaterThan(args.min);
+    expect(random).toBeLessThan(args.max);
+  })
+});
