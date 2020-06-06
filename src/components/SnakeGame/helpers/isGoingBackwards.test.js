3ecc926792cff7e306d23400224d756435482ca4
Zack Jones <zackjones11@gmail.com>
2020-06-06 23:15:19 +0200

    corrected snake helper files linting

--- src/components/SnakeGame/helpers/isGoingBackwards.test.js
+++ src/components/SnakeGame/helpers/isGoingBackwards.test.js
@@ -1,54 +1,53 @@
 import { isGoingBackwards } from "./";
-import { KEY_BIND } from "../constants";
 
-describe('isGoingBackwards.js truthy tests', () => {
-  it('returns true if current direction is DOWN and new direction is UP', () => {
-    const direction = 'down'
+describe("isGoingBackwards.js truthy tests", () => {
+  it("returns true if current direction is DOWN and new direction is UP", () => {
+    const direction = "down";
     const keyCode = 38; // up
-    expect(isGoingBackwards({direction, keyCode})).toBeTruthy();
+    expect(isGoingBackwards({ direction, keyCode })).toBeTruthy();
   });
 
-  it('returns true if current direction is UP and new direction is DOWN', () => {
-    const direction = 'up'
+  it("returns true if current direction is UP and new direction is DOWN", () => {
+    const direction = "up";
     const keyCode = 40; // down
-    expect(isGoingBackwards({direction, keyCode})).toBeTruthy();
+    expect(isGoingBackwards({ direction, keyCode })).toBeTruthy();
   });
 
-  it('returns true if current direction is LEFT and new direction is RIGHT', () => {
-    const direction = 'left'
+  it("returns true if current direction is LEFT and new direction is RIGHT", () => {
+    const direction = "left";
     const keyCode = 39; // right
-    expect(isGoingBackwards({direction, keyCode})).toBeTruthy();
+    expect(isGoingBackwards({ direction, keyCode })).toBeTruthy();
   });
 
-  it('returns true if current direction is RIGHT and new direction is LEFT', () => {
-    const direction = 'right'
+  it("returns true if current direction is RIGHT and new direction is LEFT", () => {
+    const direction = "right";
     const keyCode = 37; // left
-    expect(isGoingBackwards({direction, keyCode})).toBeTruthy();
+    expect(isGoingBackwards({ direction, keyCode })).toBeTruthy();
   });
 });
 
-describe('isGoingBackwards.js falsy tests', () => {
-  it('returns false if current direction is DOWN and new direction is LEFT', () => {
-    const direction = 'down'
+describe("isGoingBackwards.js falsy tests", () => {
+  it("returns false if current direction is DOWN and new direction is LEFT", () => {
+    const direction = "down";
     const keyCode = 37; // left
-    expect(isGoingBackwards({direction, keyCode})).toBeFalsy();
+    expect(isGoingBackwards({ direction, keyCode })).toBeFalsy();
   });
 
-  it('returns false if current direction is DOWN and new direction is RIGHT', () => {
-    const direction = 'down'
+  it("returns false if current direction is DOWN and new direction is RIGHT", () => {
+    const direction = "down";
     const keyCode = 39; // right
-    expect(isGoingBackwards({direction, keyCode})).toBeFalsy();
+    expect(isGoingBackwards({ direction, keyCode })).toBeFalsy();
   });
 
-  it('returns false if current direction is UP and new direction is LEFT', () => {
-    const direction = 'up'
+  it("returns false if current direction is UP and new direction is LEFT", () => {
+    const direction = "up";
     const keyCode = 37; // left
-    expect(isGoingBackwards({direction, keyCode})).toBeFalsy();
+    expect(isGoingBackwards({ direction, keyCode })).toBeFalsy();
   });
 
-  it('returns false if current direction is UP and new direction is RIGHT', () => {
-    const direction = 'up'
+  it("returns false if current direction is UP and new direction is RIGHT", () => {
+    const direction = "up";
     const keyCode = 39; // right
-    expect(isGoingBackwards({direction, keyCode})).toBeFalsy();
+    expect(isGoingBackwards({ direction, keyCode })).toBeFalsy();
   });
 });
--- src/components/SnakeGame/helpers/isOutside.test.js
+++ src/components/SnakeGame/helpers/isOutside.test.js
@@ -1,18 +1,18 @@
 import { isOutside } from "./";
 
-describe('isOutside.js', () => {
+describe("isOutside.js", () => {
   const numOfCells = 50;
-  it('returns false if snake is within board', () => {
+  it("returns false if snake is within board", () => {
     const snakeHead = { row: 2, column: 5 };
     expect(isOutside(numOfCells, snakeHead)).toBeFalsy();
   });
 
-  it('returns true if snake is outside board (rows)', () => {
+  it("returns true if snake is outside board (rows)", () => {
     const snakeHead = { row: numOfCells + 1, column: 5 };
     expect(isOutside(numOfCells, snakeHead)).toBeTruthy();
   });
 
-  it('returns true if snake is outside board (columns)', () => {
+  it("returns true if snake is outside board (columns)", () => {
     const snakeHead = { row: 2, column: numOfCells + 1 };
     expect(isOutside(numOfCells, snakeHead)).toBeTruthy();
   });
