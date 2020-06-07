b7cf4fbf1e8a9232e3ef9961c34405ce54d1545a
Zack Jones <zackjones11@gmail.com>
2020-06-07 11:36:24 +0200

    BaseLink unit tests

---
+++ src/components/BaseLink.test.js
@@ -0,0 +1,25 @@
+import { shallowMount } from "@vue/test-utils";
+import BaseLink from "./BaseLink.vue";
+
+describe("BaseLink.vue", () => {
+  it("should include href", () => {
+    const wrapper = shallowMount(BaseLink, {
+      propsData: {
+        href: "https://www.google.com"
+      }
+    });
+
+    expect(wrapper.find("a").attributes("href")).toBe("https://www.google.com");
+  });
+
+  it("should add download attribute", () => {
+    const wrapper = shallowMount(BaseLink, {
+      propsData: {
+        href: "./placeholder.pdf",
+        download: true
+      }
+    });
+
+    expect(wrapper.find("a").attributes("download")).toBeTruthy();
+  });
+});
