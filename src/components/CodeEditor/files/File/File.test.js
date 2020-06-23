3f6dcaeec53119ecd2f57e1228b40b1d3e3a607e
Zack Jones <zackjones11@gmail.com>
2020-06-23 22:43:40 +0200

    fixed file component unit test

--- src/components/CodeEditor/files/File/File.test.js
+++ src/components/CodeEditor/files/File/File.test.js
@@ -1,12 +1,39 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import Vuex from "vuex";
 import File from "./File.vue";
 
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+
 describe("CodeEditor/File.vue", () => {
+  let store;
+
+  beforeEach(() => {
+    const tabs = {
+      namespaced: true,
+      actions: {
+        selectTab: jest.fn()
+      },
+      getters: {
+        openTabIds: () => [1, 2]
+      }
+    };
+    store = new Vuex.Store({
+      modules: {
+        tabs
+      }
+    });
+  });
+
   it("should show code tag if a code file", () => {
     const wrapper = shallowMount(File, {
       propsData: {
+        id: 1,
         code: "<p>test</p>"
-      }
+      },
+      store,
+      localVue
     });
 
     expect(wrapper.find("code").exists()).toBeTruthy();
@@ -14,9 +41,14 @@ describe("CodeEditor/File.vue", () => {
 
   it("should display slot if defined", () => {
     const wrapper = shallowMount(File, {
+      propsData: {
+        id: 2
+      },
       slots: {
         default: "Lorem lorem lorem lorem"
-      }
+      },
+      store,
+      localVue
     });
 
     expect(wrapper.text()).toBe("Lorem lorem lorem lorem");
