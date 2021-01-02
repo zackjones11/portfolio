8996fbb7f6f9d33d6a2a24a8d0619321db8e3785
Zack Jones <zackjones11@gmail.com>
2021-01-02 22:43:30 +0100

    take into consideration months when calculating years difference

--- src/utils/yearsDifference.js
+++ src/utils/yearsDifference.js
@@ -2,6 +2,10 @@ const yearsDifference = ({ start, end }) => {
   const date1 = new Date(start);
   const date2 = end ? new Date(end) : new Date();
 
+  if (date1.getMonth() > date2.getMonth()) {
+    return date2.getFullYear() - date1.getFullYear() - 1;
+  }
+
   return date2.getFullYear() - date1.getFullYear();
 };
 
--- src/utils/yearsDifference.test.js
+++ src/utils/yearsDifference.test.js
@@ -9,18 +9,20 @@ describe("yearsDifference.js", () => {
     expect(typeof yearsSince).toBe("number");
   });
 
-  it("returns a correct years difference", () => {
-    const start = "September 1 2018";
-    const end = "September 1 2020";
-    const yearsSince = yearsDifference({ start, end });
-
-    expect(yearsSince).toBe(2);
-  });
-
   it("compares with today if no end provided", () => {
     const start = "October 1 2010";
     const yearsSince = yearsDifference({ start });
 
     expect(yearsSince).toBe(10);
+  });
+
+  test.each`
+    start                  | end                    | expected
+    ${"September 1 2018"}  | ${"September 1 2020"}  | ${2}
+    ${"September 1, 2013"} | ${"January 2, 2021"}   | ${7}
+    ${"July 2, 2010"}      | ${"September 2, 2014"} | ${4}
+  `("should return $expected years difference", ({ start, end, expected }) => {
+    const yearsSince = yearsDifference({ start, end });
+    expect(yearsSince).toBe(expected);
   });
 });
