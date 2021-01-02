6ee9a010be6aad17f21aece642da558be1e3e97d
Zack Jones <zackjones11@gmail.com>
2021-01-02 22:18:38 +0100

    call handleWindowResize on created lifecycle

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ export default {
   created() {
     this.openTab(this.$route.meta.id);
 
+    this.handleWindowResize();
     window.addEventListener("resize", this.handleWindowResize);
   },
   methods: {
