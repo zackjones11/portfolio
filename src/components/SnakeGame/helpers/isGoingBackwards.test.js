491c9325a3d511a71a0b05f1fc69dde693fb3236
Zack Jones <zackjones11@gmail.com>
2021-01-03 15:46:11 +0100

    use constant for keyCodes

--- src/components/SnakeGame/helpers/isGoingBackwards.test.js
+++ src/components/SnakeGame/helpers/isGoingBackwards.test.js
@@ -1,6 +1,13 @@
 import { DIRECTIONS } from "../constants";
 import { isGoingBackwards } from "./";
 
+const KEYS = {
+  Up: 38,
+  Down: 40,
+  Right: 39,
+  Left: 37,
+};
+
 describe("isGoingBackwards.js throw tests", () => {
   it("should throw when a direction is not supported", () => {
     const direction = "random";
@@ -15,25 +22,25 @@ describe("isGoingBackwards.js throw tests", () => {
 describe("isGoingBackwards.js truthy tests", () => {
   it("returns true if current direction is DOWN and new direction is UP", () => {
     const direction = DIRECTIONS.Down;
-    const keyCode = 38; // up
+    const keyCode = KEYS.Up;
     expect(isGoingBackwards({ direction, keyCode })).toBeTruthy();
   });
 
   it("returns true if current direction is UP and new direction is DOWN", () => {
     const direction = DIRECTIONS.Up;
-    const keyCode = 40; // down
+    const keyCode = KEYS.Down;
     expect(isGoingBackwards({ direction, keyCode })).toBeTruthy();
   });
 
   it("returns true if current direction is LEFT and new direction is RIGHT", () => {
     const direction = DIRECTIONS.Left;
-    const keyCode = 39; // right
+    const keyCode = KEYS.Right;
     expect(isGoingBackwards({ direction, keyCode })).toBeTruthy();
   });
 
   it("returns true if current direction is RIGHT and new direction is LEFT", () => {
     const direction = DIRECTIONS.Right;
-    const keyCode = 37; // left
+    const keyCode = KEYS.Left;
     expect(isGoingBackwards({ direction, keyCode })).toBeTruthy();
   });
 });
@@ -41,25 +48,25 @@ describe("isGoingBackwards.js truthy tests", () => {
 describe("isGoingBackwards.js falsy tests", () => {
   it("returns false if current direction is DOWN and new direction is LEFT", () => {
     const direction = DIRECTIONS.Down;
-    const keyCode = 37; // left
+    const keyCode = KEYS.Left;
     expect(isGoingBackwards({ direction, keyCode })).toBeFalsy();
   });
 
   it("returns false if current direction is DOWN and new direction is RIGHT", () => {
     const direction = DIRECTIONS.Down;
-    const keyCode = 39; // right
+    const keyCode = KEYS.Right;
     expect(isGoingBackwards({ direction, keyCode })).toBeFalsy();
   });
 
   it("returns false if current direction is UP and new direction is LEFT", () => {
     const direction = DIRECTIONS.Up;
-    const keyCode = 37; // left
+    const keyCode = KEYS.Left;
     expect(isGoingBackwards({ direction, keyCode })).toBeFalsy();
   });
 
   it("returns false if current direction is UP and new direction is RIGHT", () => {
     const direction = DIRECTIONS.Up;
-    const keyCode = 39; // right
+    const keyCode = KEYS.Right;
     expect(isGoingBackwards({ direction, keyCode })).toBeFalsy();
   });
 });
